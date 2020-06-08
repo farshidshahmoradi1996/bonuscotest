2eb533d8a1d6fa3971591f7c900dd8e4f76e7111
Farshid Shahmoradi <farshidshahmoradi1996@gmail.com>
2020-06-09 03:40:25 +0430

    improve

--- src/store/todos/reducer.ts
+++ src/store/todos/reducer.ts
@@ -41,6 +41,7 @@ export default function (
     case TodosActionType.UPDATE_TODO_LIST_INFO: {
       const { todos_list } = state;
       const { last_update_date, description, name, id } = action?.payload;
+      //find index by id
       const index = todos_list.findIndex((item) => item.id === id);
       todos_list[index] = {
         ...todos_list[index],
@@ -57,12 +58,15 @@ export default function (
       //find
       const { list_id, item_id } = action?.payload;
       const { todos_list } = state;
+      //find index by id
       const list_index = todos_list.findIndex((item) => item.id === list_id);
       const { todos } = todos_list[list_index];
+      //find todo index by id
       const todo_index = todos?.findIndex((item) => item.id === item_id);
-      todos_list[list_index].todos[todo_index].completed = !todos_list[
-        list_index
-      ].todos[todo_index]?.completed;
+      //toggle compeleting todo
+      const toggle = !todos_list[list_index].todos[todo_index]?.completed;
+      todos_list[list_index].todos[todo_index].completed = toggle;
+      //update date time
       todos_list[list_index].last_update_date = new Date().getTime();
       return {
         ...state,
@@ -72,10 +76,14 @@ export default function (
     case TodosActionType.ADD_NEW_TODO_ITEM: {
       const { completed, title, id, list_id } = action.payload;
       const { todos_list } = state;
+      //find index by id
       const list_index = todos_list.findIndex((item) => item.id === list_id);
       const { todos } = todos_list[list_index];
+      //add new todo
       const newTodos = [...todos, { completed, title, id }];
+      //mutate todos
       todos_list[list_index].todos = newTodos;
+      //update date time
       todos_list[list_index].last_update_date = new Date().getTime();
       return {
         ...state,
@@ -86,6 +94,7 @@ export default function (
     case TodosActionType.DELETE_LIST: {
       const { todos_list } = state;
       const { id } = action.payload;
+      //filter by id for deleteing
       const newTodosList = todos_list.filter((item) => item.id !== id);
       return {
         ...state,
@@ -98,10 +107,15 @@ export default function (
         list_id,
         todos_id,
       }: { list_id: number; todos_id: number[] } = action.payload;
+
+      //find index by id
       const index = todos_list.findIndex((item) => item.id === list_id);
+
+      //filter by exist id list
       todos_list[index].todos = todos_list[index].todos.filter(
         (_) => !todos_id.includes(_.id)
       );
+      //update time
       todos_list[index].last_update_date = new Date().getTime();
       return {
         ...state,
